8857b53e9618cb711300b1491c1481406fbf1f6b
anjudrake <anju.drake@hotmail.com>
2016-11-08 11:36:44 +0530

    settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Subscription } from '../../Stubs/Subscription';
 
 /*
   Generated class for the Settings page.
@@ -9,11 +10,44 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   selector: 'page-settings',
-  templateUrl: 'settings.html'
+  templateUrl: 'settings.html',
+  providers: [Subscription]
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {}
+  chanelList = [];
+  subscribeList = [];
+
+  constructor(public navCtrl: NavController, private subscription: Subscription) {
+    this.InvalidateSubscribeListAsync();
+    this.InvalidateChannelListAsync();
+  }
+
+  InvalidateSubscribeListAsync() {
+    this.subscribeList = this.subscription.GetSubscriptionList();
+  }
+
+  InvalidateChannelListAsync() {
+    var ChanelList = this.subscription.GetChannelsAsync();
+    ChanelList.forEach(channel => {
+      var value = this.subscribeList.find(x => x.ChannelName == channel.ChannelName)
+      if (value == undefined) {
+        this.chanelList.push(channel);
+      }
+    });
+  }
+
+  SubscribeList(index, channelName) {
+    var channel = this.subscription.RequestSubscriptionAsync(channelName);
+    this.subscribeList.push(channel);
+    this.chanelList.splice(index, 1);
+  }
+
+  UnSubscribeList(index) {
+    this.subscribeList.splice(index, 1);
+    this.chanelList=[];
+    this.InvalidateChannelListAsync();
+  }
 
   ionViewDidLoad() {
     console.log('Hello Settings Page');
