d555a2704984cd125c114d0dbd73390319d32836
gurpreetdrake <gurpreet.drake@hotmail.com>
2016-11-22 19:09:05 +0530

    IP Cam recording Done

--- src/pages/connectivity/connectivity.ts
+++ src/pages/connectivity/connectivity.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController } from 'ionic-angular';
-
-import { } from 'ionic-native';
+import { NavController } from 'ionic-angular';
 
 import { Connection } from '../../pages/Connection';
-
-import X2JS from 'x2js';
-import { Http, Headers } from '@angular/http';
 
 /*
   Generated class for the Connectivity page.
@@ -32,9 +27,9 @@ export class Connectivity {
     isConnectivityEnabled: boolean = true;
 
     constructor(public navCtrl: NavController,
-        private connection: Connection,
-        private alertCtrl: AlertController,
-        private http: Http) { }
+        private connection: Connection) {
+
+    }
 
     ionViewDidLoad() {
         console.log('Hello Connectivity Page');
@@ -80,61 +75,6 @@ export class Connectivity {
         if (status == "Unavailable") {
             return 'gray';
         }
-    }
-
-    ipCams = [];
-
-    previewIPCams() {
-        this.ipCams.length = 0;
-
-        if (Connection.connectedServer == null) {
-            let alert = this.alertCtrl.create({
-                title: 'Not connected!',
-                subTitle: 'Not connected to any server',
-                buttons: ['OK']
-            });
-            alert.present();
-        }
-        else {
-            var connectedServerIP = Connection.connectedServer.Address;
-            var Port = Connection.connectedServer.Port;
-
-            var requestUri = "http://" + connectedServerIP + ":10080/icamera/cams/ip/";
-            console.log('Requesting: ' + requestUri);
-
-            this.http.get(requestUri)
-                .map(res => res.text())
-                .subscribe(
-                data => {
-                    console.log('data: ' + data)
-                    let parser: any = new X2JS();
-                    var jsonData = parser.xml2js(data);
-                    console.log(jsonData.Cams);
-                    this.ipCams = jsonData.Cams.IPCam;
-                },
-                err => console.error('There was an error: ' + err),
-                () => console.log('Random Quote Complete')
-                );
-        }
-    }
-
-    stringifyJSON(data) {
-        return JSON.stringify(data);
-    }
-
-
-    getIPCamPreview(ipAdd) {
-        var requestUri = "http://" + ipAdd + "/Streaming/channels/101/picture"
-
-        this.http.get(requestUri)
-            .map(res => res.text())
-            .subscribe(
-            data => {
-                console.log('data: ' + data)
-            },
-            err => console.error('There was an error: ' + err),
-            () => console.log('Random Quote Complete')
-            );
     }
 }
 
--- src/pages/editor/ipcameras/ipcameras.ts
+++ src/pages/editor/ipcameras/ipcameras.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController, ViewController } from 'ionic-angular';
+import { NavController, ModalController, ViewController, AlertController, NavParams, LoadingController } from 'ionic-angular';
+
+import { Http } from '@angular/http';
+import X2JS from 'x2js';
+
+import { Connection } from '../../../pages/Connection';
+import { Connectivity } from '../../connectivity/connectivity';
 
 /*
   Generated class for the Ipcamera page.
@@ -14,18 +20,77 @@ import { NavController, ModalController, ViewController } from 'ionic-angular';
 export class Ipcameras {
 
   constructor(public navCtrl: NavController,
-    private modalCtrl: ModalController) { }
+    private modalCtrl: ModalController,
+    private http: Http,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController) { }
 
   ionViewDidLoad() {
     console.log('Hello Ipcamera Page');
   }
 
+  ionViewDidEnter() {
+    this.loadIPCams();
+  }
+
+
+  openConnectivity() {
+    this.navCtrl.push(Connectivity);
+  }
+
+  ipCams = [];
+
+  loadIPCams() {
+    this.ipCams.length = 0;
+
+    if (Connection.connectedServer == null) {
+      let alert = this.alertCtrl.create({
+        title: 'Not connected!',
+        message: 'Please connect to an available server.',
+        buttons: [
+          {
+            text: 'Connect',
+            handler: () => {
+              this.openConnectivity();
+            }
+          },
+          {
+            text: 'Cancel',
+            handler: () => {
+              alert.dismiss();
+            }
+          }
+        ]
+      });
+      alert.present();
+    }
+    else {
+      var connectedServerIP = Connection.connectedServer.Address;
+      var Port = Connection.connectedServer.Port;
+
+      var requestUri = "http://" + connectedServerIP + ":10080/icamera/cams/ip/";
+
+      this.http.get(requestUri)
+        .map(res => res.text())
+        .subscribe(
+        data => {
+          let parser: any = new X2JS();
+          var jsonData = parser.xml2js(data);
+          this.ipCams = jsonData.Cams.IPCam;
+        },
+        err => console.error('There was an error: ' + err),
+        () => console.log('Random Quote Complete')
+        );
+    }
+  }
+
+  getIPCamPreview(ipAdd: string) {
+    return "http://" + ipAdd + "/Streaming/channels/101/picture";
+  }
+
   isTimerOn: boolean = false;
 
-  presentSettingsModal() {
-    let modal = this.modalCtrl.create(IpCamSettingsModal);
-    modal.present();
-  }
+  timerDelay: number = 3;
 
   timerButtonOpacity: Number = 0.5;
 
@@ -40,23 +105,145 @@ export class Ipcameras {
     }
   }
 
-  startRecording(){
-    
+  presentSettingsModal() {
+    let modal = this.modalCtrl.create(IpCamSettingsModal, {
+      timerDelay: this.timerDelay
+    });
+    modal.present();
+
+    modal.onDidDismiss(TimerDelay => {
+      this.timerDelay = TimerDelay;
+    })
+  }
+
+  recordingDuration: number = 5;
+
+  isRecording: boolean = false;
+
+  startRecording() {
+    this.isRecording = true;
+    if (this.isTimerOn) {
+      var time = Number(this.timerDelay);
+
+      let loader = this.loadingCtrl.create({
+        content: 'Wait ' + time.toString() + 's',
+        duration: this.timerDelay * 1000,
+        dismissOnPageChange: true
+      });
+      loader.present();
+
+      var interval = setInterval(() => {
+        time--;
+        loader.setContent('Wait ' + time.toString() + 's');
+        if (time <= 0) {
+          clearInterval(interval);
+          this.record();
+        }
+      }, 1000)
+    }
+    else {
+      this.record();
+    }
+  }
+
+  record() {
+    console.log("Recording for IP Cams on network");
+    var connectedServerIP = Connection.connectedServer.Data.Location;
+    var Name = Date.now();
+    var uri: string = "http://" + connectedServerIP + ":10080/icamera/cams/ip/" + Name + "/rec?duration=" + this.recordingDuration;
+    console.log('Request URI: ' + uri);
+    this.http.post(uri, null)
+      .toPromise()
+      .then(res => {
+        console.log('Response: ' + res);
+          var time = Number(0);
+
+          let loader = this.loadingCtrl.create({
+            content: 'Recording ' + time.toString() + 's',
+            duration: this.recordingDuration * 1000,
+            dismissOnPageChange: true
+          });
+          loader.present();
+
+          var interval = setInterval(() => {
+            time++;
+            loader.setContent('Recording ' + time.toString() + 's');
+            if (time >= this.recordingDuration) {
+              clearInterval(interval);
+              this.isRecording = false;
+            }
+          }, 1000);
+      })
+      .catch(err => {
+        console.log('Error: ' + err);
+        let alert = this.alertCtrl.create({
+          title: 'Error',
+          subTitle: err,
+          buttons: ['OK']
+        });
+        alert.present();
+        this.isRecording = false;
+      });
   }
 }
 
 @Component({
   selector: 'page-IpCamsSettingsModal',
   template: `
-  <ion-header no-shadow >
+  <ion-header no-shadow>
+
+  <ion-toolbar no-border-bottom>
+    <ion-title color="primary">IP Cam Settings</ion-title>
+    <ion-buttons end>
+      <button ion-button color="primary" (click)="dismiss()">OK</button>
+    </ion-buttons>
+  </ion-toolbar>
+
 </ion-header>
-<ion-content>
-    </ion-content>
+<ion-content padding style=" ::-webkit-scrollbar,*::-webkit-scrollbar: display: none;">
+
+<ion-item>
+<ion-label item-left fixed>Timer ({{timerDelay}}s)</ion-label>
+<ion-range item-right min="3" max="15" step="3" snaps="true" [(ngModel)]="timerDelay">
+<ion-label range-left>3s</ion-label>
+<ion-label range-right>15s</ion-label>
+</ion-range>
+</ion-item>
+<ion-item>
+<ion-label item-left fixed>Brightness</ion-label>
+<ion-range>
+<ion-icon range-left small name="sunny"></ion-icon>
+<ion-icon range-right name="sunny"></ion-icon>
+</ion-range>
+</ion-item>
+<ion-item>
+<ion-label item-left fixed>AWB</ion-label>
+<ion-range>
+<ion-icon range-left small name="contrast"></ion-icon>
+<ion-icon range-right name="contrast"></ion-icon>
+</ion-range>
+</ion-item>
+<ion-item>
+<ion-label item-left fixed>Zoom</ion-label>
+<ion-range>
+<ion-icon range-left small name="search"></ion-icon>
+<ion-icon range-right name="search"></ion-icon>
+</ion-range>
+</ion-item>
+
+</ion-content>
   `
 })
 export class IpCamSettingsModal {
 
-  constructor(public viewCtrl: ViewController) {
+  timerDelay: number;
 
+  constructor(public viewCtrl: ViewController,
+    private navParams: NavParams) {
+    this.timerDelay = navParams.data.timerDelay;
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss(this.timerDelay);
   }
 }
