dc97e434eda79a45a9f046dfd0f7022ae05d833b
anjudrake <anju.drake@hotmail.com>
2016-11-26 17:55:24 +0530

    header

--- src/Stubs/Subscription.ts
+++ src/Stubs/Subscription.ts
@@ -7,22 +7,35 @@ export class Subscription {
         console.log("stubs......")
     }
 
-    GetChannelsAsync() {
-        var channelsList = [
-            { ChannelName: "SportsPIP", IsPrivate: false, Description: "" },
-            { ChannelName: "Harvest", IsPrivate: false, Description: "" },
-            { ChannelName: "Mayfair", IsPrivate: false, Description: "" },
-        ];
-        return channelsList
+    GetChannelsAsync(userid) {
+        var channelList = [];
+        return this.http.get("http://sportspipservice.cloudapp.net:10106/IMobile/channels/list?uid=" + userid + "")
+            .map(res => res.json())
+            .map(us => {
+                var data = JSON.parse(us);
+                data.Returns.forEach(ch => {
+                    var ChList = { ChannelName: ch.Name, IsPrivate: ch.IsPrivate, Description: ch.Description }
+                    channelList.push(ChList);
+                });
+                return channelList;
+            }).toPromise();
     }
 
-    GetSubscriptionList() {
-        var subscription = [
-            { ChannelName: "SportsPIP", IsPrivate: false, Description: "Football-Handball", IsAuthorized: true, DtExpiry: new Date(), IsMatrixUploader: true },
-            { ChannelName: "Harvest", IsPrivate: false, Description: "Tennis,Badminton", IsAuthorized: true, DtExpiry: new Date(), IsMatrixUploader: false }
-        ];
-
-        return subscription;
+    GetSubscriptionList(userid) {
+        return this.http.get("http://sportspipservice.cloudapp.net:10106/IMobile/subscriptions/list?uid=" + userid + "")
+            .map(res => res.json())
+            .map(us => {
+                var data = JSON.parse(us);
+                if (data instanceof Array) {
+                    data.forEach(sbchn => {
+                        console.log(sbchn);
+                    });
+                    // var subscription = [
+                    //     { ChannelName: "SportsPIP", IsPrivate: false, Description: "Football-Handball", IsAuthorized: true, DtExpiry: new Date(), IsMatrixUploader: true },
+                    //     { ChannelName: "Harvest", IsPrivate: false, Description: "Tennis,Badminton", IsAuthorized: true, DtExpiry: new Date(), IsMatrixUploader: false }
+                    // ];
+                }
+            }).toPromise();
     }
 
     RequestSubscriptionAsync(channelName) {
@@ -32,19 +45,26 @@ export class Subscription {
     }
 
     RegisterAsync(firstName, lastName, email) {
-        this.http.get("http://sportspipservice.cloudapp.net:10106/IMobile/users/{" + firstName + "}/{" + lastName + "}/{" + email + "}/{@abc123}/{0}")
-            .subscribe(data => {
-                console.log(data.text());
-            })
-        // var user =
-        //     { FirstName: firstName, LastName: lastName, Email: email, Password: "@abc123" };
-        // return user;
+        console.log(firstName, lastName, email);
+        return this.http.get("http://sportspipservice.cloudapp.net:10106/IMobile/users/" + firstName + "/" + lastName + "/" + email + "/abc123/0")
+            .map(res => res.json())
+            .map(us => {
+                console.log(us);
+                var data = JSON.parse(us);
+                console.log(data);
+                var user = { Name: firstName + " " + lastName, FirstName: firstName, LastName: lastName, Email: email, UserId: data.ID };
+                return user;
+            }).toPromise();
     }
 
     LoginAsync(email, password) {
-
-        var user = { FirstName: "Sachin", LastName: "Tendulkar", Email: email }
-        return user;
+        return this.http.get("http://sportspipservice.cloudapp.net:10106/IMobile/users/" + email + "/" + password + "/0")
+            .map(res => res.json())
+            .map(us => {
+                var data = JSON.parse(us);
+                var user = { Name: data.Returns.Name, FirstName: data.Returns.FirstName, LastName: data.Returns.LastName, Email: data.Returns.Email, UserId: data.Returns.ID }
+                return user;
+            }).toPromise();
     }
 
 }
