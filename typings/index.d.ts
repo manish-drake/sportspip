6b7708de4a2e56d6c00a49e9d821e6911042ca31
gurpreetdrake <gurpreet.drake@hotmail.com>
2016-12-17 17:27:31 +0530

    typings dir ignored

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/cordova/index.d.ts" />
-/// <reference path="globals/require/index.d.ts" />
-/// <reference path="globals/xml2js/index.d.ts" />
-/// <reference path="modules/debug/index.d.ts" />
