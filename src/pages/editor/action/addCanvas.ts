7b3d8a52a3204de6b176d152c4ad791d782deddc
anjudrake <anju.drake@hotmail.com>
2017-01-06 13:33:33 +0530

    editor refactor

--- src/pages/editor/action/addCanvas.ts
+++ src/pages/editor/action/addCanvas.ts
@@ -7,6 +7,19 @@ export class AddCanvas implements ICommand {
 
     }
     run(cmdArgs) {
-
+        var canvasView = {
+            "Content": {
+                "PIP": {
+                    "PIP.Objects": "",
+                    "_name": "d002b8ed5fe24f57aab501f05398262c",
+                    "_CanvasBackgroundBrush": "#FFFFFFFF",
+                    "_CanvasBackgroundOpacity": "1"
+                }
+            },
+            "_name": "View " + (cmdArgs.editor.selectedViewIndex + 1),
+            "_Title": "View " + (cmdArgs.editor.selectedViewIndex + 1),
+            "_Source": 'Canvas'
+        }
+        cmdArgs.editor.views[cmdArgs.editor.selectedViewIndex] = canvasView;
     }
 }
--- src/pages/editor/action/addPhoneCapture.ts
+++ src/pages/editor/action/addPhoneCapture.ts
@@ -1,12 +1,63 @@
 import { Injectable, Inject } from '@angular/core';
 import { ICommand } from '../../../Contracts/ICommand';
+import { CaptureVideoOptions, MediaFile, CaptureError, FilePath, MediaCapture } from 'ionic-native';
+import { Logger } from '../../../logging/logger';
+import { AlertControllers } from '../../../Services/Alerts';
+import { BackGroundTransferProcess } from '../../../Services/BackGroundTransferProcess';
+import { StorageFactory } from '../../../Services/Factory/StorageFactory';
+import { Platform } from 'ionic-angular';
+import { Connection } from '../../../Services/Connection';
+import { Storage } from '../../../Services/Factory/Storage';
+
 
 @Injectable()
 export class AddPhoneCapture implements ICommand {
-    constructor() {
+
+    rootDirectory: string;
+    constructor(private alertCtrls: AlertControllers,
+        private _logger: Logger,
+        private backGroundTransferProcess: BackGroundTransferProcess,
+        private platform: Platform,
+        private storage: Storage,
+        private storagefactory: StorageFactory) {
+        this.rootDirectory = this.storage.externalRootDirectory();
 
     }
     run(cmdArgs) {
+        let options: CaptureVideoOptions = {};
+        MediaCapture.captureVideo(options)
+            .then(
+            (data: MediaFile[]) => { this.captureSuccess(data,cmdArgs) },
+            (err: CaptureError) => { this.captureError(err) }
+            );
+    }
 
+    captureSuccess(MediaFiles, Model) {
+        MediaFiles.forEach(mediaFile => {
+            var fileUrl = mediaFile.localURL;
+
+            var path = fileUrl.substr(0, fileUrl.lastIndexOf('/') + 1);
+            var fileName = fileUrl.substr(fileUrl.lastIndexOf('/') + 1);
+
+            this.storagefactory.MoveFile(path, this.rootDirectory + "SportsPIP/Video", fileName)
+                .subscribe(success => {
+                    Model.editor.CreateVideoView(fileName);
+                    console.log('Successfully saved video')
+                    if (Connection.connectedServer != null)
+                        this.backGroundTransferProcess.TransferVideo(fileName, Connection.connectedServer.Address, Model.editor.views);
+                })
+        });
+    }
+
+    captureError(err) {
+        if (err.code == "3") {
+            console.log("Reording: " + err.message + ", Code:" + err.code)
+            this._logger.Error("Reording: ", err.message)
+            this._logger.Error("Code:", err.code)
+        }
+        else {
+            this._logger.Error('Recording Failed!', err.code + ", " + err.message);
+            this.alertCtrls.BasicAlert('Recording Failed!', err.code + ", " + err.message);
+        }
     }
 }
