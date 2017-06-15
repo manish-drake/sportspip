5afba2156a11ad411d0096f06cc1a1c0781817bc
gurpreetdrake <gurpreet.drake@hotmail.com>
2017-06-15 13:26:20 +0530

    back button confirmation undone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, ToastController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
@@ -14,34 +14,12 @@ export class sportspip {
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
-        private statusBar: StatusBar,
-        private toastCtrl: ToastController) {
+        private statusBar: StatusBar) {
         platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             this.statusBar.styleDefault();
             this.splashScreen.hide();
-            if (this.platform.is('android')) {
-                //back button handle
-                //Registration of push in Android
-                var lastTimeBackPress = 0;
-                var timePeriodToExit = 2000;
-
-                this.platform.registerBackButtonAction(() => {
-                    //Double check to exit app
-                    if (new Date().getTime() - lastTimeBackPress < timePeriodToExit) {
-                        this.platform.exitApp(); //Exit from app
-                    } else {
-                        let toast = this.toastCtrl.create({
-                            message: 'Press again to exit..',
-                            duration: 3000,
-                            position: 'bottom'
-                        });
-                        toast.present();
-                        lastTimeBackPress = new Date().getTime();
-                    }
-                });
-            }
         });
     }
 
