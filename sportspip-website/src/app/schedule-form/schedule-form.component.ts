55e092b2e61e12f4ce68a9f83e1d4c820ae2db99
manish <manish@drake.in>
2020-07-06 17:29:34 +0530

    Event form corrected and nav bar changed

--- sportspip-website/src/app/schedule-form/schedule-form.component.ts
+++ sportspip-website/src/app/schedule-form/schedule-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { OverlayHandle } from 'src/app/overlay-handle';
-import { FormBuilder, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {OverlayHandle} from 'src/app/overlay-handle';
+
 /*
 export interface ScheduleItem {
   eventType: string;
@@ -18,21 +19,24 @@ export class ScheduleFormComponent implements OnInit {
   frmSubscribe = this.fb.group({
     eventType: 'John Doe',
     opponent: 'Team X',
-    eventDatetime: '2020-10-20',
-    location: 'Liverpool'
+    eventDatetime: new Date('2020-10-20'),
+    location: 'Liverpool',
+    id: ''
   });
   eventTypes: string[] = ['Season', 'Profession', 'Tournament'];
   constructor(private fb: FormBuilder, private ref: OverlayHandle) {
-    this.frmSubscribe = this.fb.group({
-      eventType: ref.data.EventType,
-      opponent: ref.data.Opponent,
-      eventDatetime: ref.data.EventDate,
-      location: ref.data.Location
-    });
+    if (ref.data !== null) {
+      this.frmSubscribe = this.fb.group({
+        eventType: ref.data.EventType,
+        opponent: ref.data.Opponent,
+        eventDatetime: ref.data.EventDate,
+        location: ref.data.Location,
+        id: ref.data.id
+      });
+    }
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   submit() {
     this.ref.close(this.frmSubscribe.value);
   }
--- sportspip-website/src/app/schedule/schedule.component.ts
+++ sportspip-website/src/app/schedule/schedule.component.ts
@@ -1,11 +1,13 @@
-import { AfterViewInit, Component, OnInit, ViewChild, TemplateRef, Type } from '@angular/core';
-import { OverlayService } from '../overlay.service';
-import { ComponentType } from '@angular/cdk/portal';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTable, MatTableDataSource } from '@angular/material/table';
-import { ScheduleFormComponent } from '../schedule-form/schedule-form.component';
-import { ApiService } from '../service/api.service';
+import {ComponentType} from '@angular/cdk/portal';
+import {AfterViewInit, Component, OnInit, TemplateRef, Type, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+
+import {OverlayService} from '../overlay.service';
+import {ScheduleFormComponent} from '../schedule-form/schedule-form.component';
+import {ApiService} from '../service/api.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 export interface ScheduleItem {
   EventType: string;
@@ -27,10 +29,19 @@ export class ScheduleComponent implements AfterViewInit, OnInit {
   dataSource: MatTableDataSource<ScheduleItem>;
   crudComponent: Type<any> = ScheduleFormComponent
 
-  constructor(private overlayService: OverlayService, private apiService: ApiService) { }
-
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['EventType', 'Opponent', 'EventDate', 'Location', 'DeleteAction'];
+  constructor(
+      private overlayService: OverlayService, private apiService: ApiService, private _snackBar: MatSnackBar) {}
+      openSnackBar(message: string, action: string) {
+        this._snackBar.open(message, action, {
+          duration: 2000,
+        });
+      }
+  /**
+   * Columns displayed in the table. Columns IDs can be added, removed, or
+   * reordered.
+   */
+  displayedColumns =
+      ['EventType', 'Opponent', 'EventDate', 'Location', 'DeleteAction'];
   isOpen = false;
   loadingData: Boolean = false;
   ngOnInit() {
@@ -56,24 +67,63 @@ export class ScheduleComponent implements AfterViewInit, OnInit {
     this.dataSource.paginator = this.paginator;
     this.table.dataSource = this.dataSource;
   }
-  open(content: TemplateRef<any> | ComponentType<any> | string, data: any = {}) {
+  open(content: TemplateRef<any>|ComponentType<any>|string, data: any = {}) {
     const ref = this.overlayService.open(content, data);
 
     ref.afterClosed$.subscribe(res => {
-      let modData = res.data;
-     // console.log(modData);
-      data.EventType = modData.eventType;
-      data.Opponent = modData.opponent;
-      data.EventDate = modData.eventDatetime;
-      data.Location = modData.location;
-      this.apiService.updateItem("events", data).subscribe(result => {
-        // console.log("success");
-        // console.log(result);
-        let resp = result;
-      },
-        (error) => {
-          console.log("Error; update events data: ", error);
-        });
+      let formData = res.data;
+      // console.log(formData);
+      if (data === null) {
+        this.apiService.addItem('events', JSON.stringify({
+          EventType: formData.eventType,
+          Opponent: formData.opponent,
+          EventDate: formData.eventDatetime,
+          Location: formData.location
+        }))
+            .subscribe(
+                result => {
+                  let resp = result;
+                  let dataSlice = this.dataSource.data.slice();
+                  dataSlice.push(resp as any);
+                  this.dataSource.data = dataSlice;
+                },
+                (error) => {
+                  console.log('Error; update events data: ', error);
+                });
+      } else {
+        this.apiService.updateItem('events', data)
+            .subscribe(
+                result => {
+                  let resp = result;
+                  data.EventType = formData.eventType;
+                  data.Opponent = formData.opponent;
+                  data.EventDate = formData.eventDatetime;
+                  data.Location = formData.location;
+                },
+                (error) => {
+                  console.log('Error; update events data: ', error);
+                });
+      }
     });
+  }
+  delete(id: any) {
+    console.log(id);
+    this.loadingData = true;
+    this.apiService.deleteItem('events', id)
+        .subscribe(
+            (data) => {
+              // console.log(data);
+              this.openSnackBar('Item deleted successfully!', '');
+              let dataSlice = this.dataSource.data.slice();
+              let idx = dataSlice.findIndex(item=>item.id === id);
+              dataSlice.splice(idx, 1);
+              this.dataSource.data = dataSlice;
+              this.loadingData = false;
+            },
+            (error) => {
+              this.openSnackBar('Error; delete Item!', '');
+              console.log('Error; delete players data: ', error);
+              this.loadingData = false;
+            });
   }
 }
