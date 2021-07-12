0d9c10f8f515b9a6c52dafed72eeefa2f6bc4ff7
rakesh406c <rakesh@drake.co.in>
2021-07-12 18:00:02 +0530

    still need work on coache filter

--- src/app/main/sportspip/coaches-filter/coachesapi.service.ts
+++ src/app/main/sportspip/coaches-filter/coachesapi.service.ts
@@ -1,24 +1,38 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ICoaches } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoachesapiService {
-  private _urlCoaches : string = "http://drake.in:1337/coaches";
-  private _urlFilter : string = "http://drake.in:1337/Game-Filters";
+  // private _urlCoaches : string = "http://drake.in:1337/coaches";
+  // private _urlFilter : string = "http://drake.in:1337/Game-Filters";
 
-  constructor(private _httpClient: HttpClient) { }
+  private _urlFilter : string ="http://192.168.10.50:1337/Game-Filters";
+  private _urlCoaches : string = "http://192.168.10.50:1337/coaches";
 
-  // getCoache(): Observable<ICoaches[]> {
-  //   return this._httpClient.get<ICoaches[]>(this._urlCoaches);
-  // }
+  public tempEvents;
+  public events;
+  public coaches;
+
+
+  public onCoachesChange: BehaviorSubject<any>;
+  onEventChange: any;
+
+  constructor(private _httpClient: HttpClient,
+    ) {
+      this.onCoachesChange = new BehaviorSubject({});
+     }
+
+  
    coachesData(){
     return this._httpClient.get(this._urlCoaches);
   }
    filterData(){
     return this._httpClient.get(this._urlFilter);
   }
+
+  
 }
