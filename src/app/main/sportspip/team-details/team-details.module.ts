14d7e453492b0550c16a8ba9f57ae7d6d581ced0
rajadrake <karan@drake.co.in>
2021-07-17 12:12:43 +0530

    Teams UI modifications

--- src/app/main/sportspip/team-details/team-details.module.ts
+++ src/app/main/sportspip/team-details/team-details.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamDetailsComponent } from './team-details.component';
 import { RouterModule, Routes } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CoreCommonModule } from '@core/common.module';
+import { ContentHeaderModule } from 'app/layout/components/content-header/content-header.module';
+import { FormsModule } from 'app/main/forms/forms.module';
 
 // routing
 const routes: Routes = [
@@ -12,10 +16,12 @@ const routes: Routes = [
 @NgModule({
   declarations: [TeamDetailsComponent],
   imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
- 
+    CommonModule, RouterModule.forChild(routes),
+  NgbModule, CoreCommonModule,
+  ContentHeaderModule,
+  FormsModule
   ],
+  
   exports:[RouterModule],
 })
 export class TeamDetailsModule { }
